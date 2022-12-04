285a5132131e802609adef31bd4cea64b101809e
paulo <paulosergio3212010@hotmail.com>
2022-12-04 18:28:59 -0300

    adjusts

--- src/jvmMain/kotlin/presentation/ui/screens/GameboardScreen.kt
+++ src/jvmMain/kotlin/presentation/ui/screens/GameboardScreen.kt
@@ -1,13 +1,18 @@
 package presentation.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
 @Composable
 fun CardboardScreen() {
     Image(
         painter = painterResource("images/gameboard_background.png"),
-        contentDescription = null
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.FillBounds
     )
 }
