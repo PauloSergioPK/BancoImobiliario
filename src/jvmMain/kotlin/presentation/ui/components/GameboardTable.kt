8899df2875713995cf3a7469332ead0c2708f91f
paulo <paulosergio3212010@hotmail.com>
2022-12-07 20:58:04 -0300

    iniciado tabuleiro

---
+++ src/jvmMain/kotlin/presentation/ui/components/GameboardTable.kt
@@ -0,0 +1,13 @@
+package presentation.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import domain.model.gameboard.Gameboard
+
+@Composable
+fun GameboardTable(
+    gameboard: Gameboard,
+    modifier: Modifier = Modifier
+) {
+
+}
--- src/jvmMain/kotlin/presentation/ui/screens/GameboardScreen.kt
+++ src/jvmMain/kotlin/presentation/ui/screens/GameboardScreen.kt
@@ -2,61 +2,30 @@ package presentation.ui.screens
 
 import androidx.compose.runtime.Composable
 import domain.model.colors.GameColor
+import domain.model.gameboard.Gameboard
 import domain.model.gameboard.GameboardHouse
 import domain.model.gameboard.GameboardHouseKind
 import domain.model.player.BankAccount
 import domain.model.player.Player
-import presentation.ui.components.GameboardHouseCard
+import presentation.ui.components.GameboardTable
 
 @Composable
 fun CardboardScreen() {
-    val gameboardHouse = GameboardHouse(
-        id = "1",
-        players = listOf(
-            Player(
-                name = "joao",
-                bankAccount = BankAccount(
-                    balance = 1000F
-                ),
-                color = GameColor.RED
-            ),
-            Player(
-                name = "joao",
-                bankAccount = BankAccount(
-                    balance = 1000F
-                ),
-                color = GameColor.RED
-            ),
-            Player(
-                name = "joao",
-                bankAccount = BankAccount(
-                    balance = 1000F
-                ),
-                color = GameColor.RED
-            ),
-            Player(
-                name = "joao",
-                bankAccount = BankAccount(
-                    balance = 1000F
-                ),
-                color = GameColor.RED
-            ),
-            Player(
-                name = "joao",
-                bankAccount = BankAccount(
-                    balance = 1000F
-                ),
-                color = GameColor.RED
-            ),
-            Player(
-                name = "joao",
-                bankAccount = BankAccount(
-                    balance = 1000F
-                ),
-                color = GameColor.RED
+    val fakeList = mutableListOf<GameboardHouse>()
+    repeat(44) { index ->
+        fakeList.add(
+            GameboardHouse(
+                id = "1",
+                players = if (index == 0) {
+                    listOf(
+                        Player("nome", BankAccount(balance = 1000F), color = GameColor.GREEN)
+                    )
+                } else listOf(),
+                kind = GameboardHouseKind.GoToJail
             )
-        ),
-        kind = GameboardHouseKind.GoToJail
-    )
-    GameboardHouseCard(gameboardHouse)
+        )
+    }
+    val gameboard = Gameboard(fakeList)
+    GameboardTable(gameboard)
 }
+
