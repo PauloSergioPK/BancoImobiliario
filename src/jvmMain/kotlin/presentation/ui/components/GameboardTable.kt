8e2d8feb16da04b224c05614a69f436d31f0bc84
paulo <paulosergio3212010@hotmail.com>
2022-12-07 21:01:32 -0300

    iniciado tabuleiro

--- src/jvmMain/kotlin/presentation/ui/components/GameboardTable.kt
+++ src/jvmMain/kotlin/presentation/ui/components/GameboardTable.kt
@@ -1,5 +1,8 @@
 package presentation.ui.components
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import domain.model.gameboard.Gameboard
@@ -9,5 +12,9 @@ fun GameboardTable(
     gameboard: Gameboard,
     modifier: Modifier = Modifier
 ) {
-
+    LazyHorizontalGrid(rows = GridCells.Fixed(11), modifier = Modifier.fillMaxSize()) {
+        gameboard.houses.forEach {
+            item { GameboardHouseCard(it) }
+        }
+    }
 }
