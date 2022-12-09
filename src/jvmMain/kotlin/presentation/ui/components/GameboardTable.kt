11819b8f9b9b9ee00cfc87ebe8f2ffe968b6ca4e
paulo <paulosergio3212010@hotmail.com>
2022-12-08 22:47:08 -0300

    ajustes tabuleiro

--- src/jvmMain/kotlin/presentation/ui/components/GameboardTable.kt
+++ src/jvmMain/kotlin/presentation/ui/components/GameboardTable.kt
@@ -1,12 +1,15 @@
 package presentation.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import domain.model.gameboard.Gameboard
 import presentation.APP_NAME
 
@@ -16,15 +19,15 @@ fun GameboardTable(
     modifier: Modifier = Modifier
 ) {
     val bottomLine = gameboard.houses.take(11).reversed()
-    val leftLine = gameboard.houses.drop(11).take(10).reversed()
-    val topLine = gameboard.houses.drop(11).drop(10).take(11)
-    val rightLine = gameboard.houses.drop(11).drop(10).drop(11).take(10)
+    val leftLine = gameboard.houses.drop(11).take(9).reversed()
+    val topLine = gameboard.houses.drop(11).drop(9).take(11)
+    val rightLine = gameboard.houses.drop(11).drop(9).drop(11).take(9)
 
     BoxWithConstraints(modifier = Modifier.fillMaxSize().then(modifier)) {
 
         Column(modifier = Modifier.fillMaxSize()) {
 
-            val cardModifier =  Modifier.fillMaxSize().weight(1F)
+            val cardModifier =  Modifier.fillMaxSize().border(BorderStroke(1.dp, Color.Black)).weight(1F)
 
             Row(
                 modifier = Modifier.fillMaxWidth().weight(0.1F),
